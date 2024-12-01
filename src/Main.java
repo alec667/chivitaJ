0723009f65e42b69dcd3626669dcfe1fc09db9ac
Alejandro Casiva <alec667@gmail.com>
2024-11-30 23:22:41 -0300

    agrega 100 elementos al array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import static java.lang.Math.random;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,7 +14,7 @@ public class Main {
                 "La águila", "El cuervo", "La golondrina", "El halcón", "La paloma", "El ratón", "La rata", "El búho",
                 "La lechuza", "El pingüino", "El canguro", "La jirafa", "El rinoceronte", "El hipopótamo", "La ardilla",
                 "El castor", "La marmota", "El zorro", "La mofeta", "El tejón", "La nutria", "El mapache", "El puma",
-                "La pantera", "El guepardo", "El chacal", "La hiena", "El lémur", "La zarigüeya", "El walabí", "El koala",
+                "La pantera", "El guepardo", "El chacal", "La hiena", "El lémur", "La zarigüeya", "El wallaby", "El koala",
                 "La tortuga", "El cocodrilo", "El caimán", "El lagarto", "La iguana", "El camaleón", "La serpiente",
                 "La anaconda", "La boa", "La víbora", "El escorpión", "La araña", "El ciempiés", "El milpiés", "La hormiga",
                 "La abeja", "La avispa", "El saltamontes", "La langosta", "El grillo", "La mariposa", "El gusano", "La oruga",
@@ -24,12 +26,13 @@ public class Main {
         Map<String, String> llamarA = new HashMap<>();
 
         System.out.println("Sal de ahí chivita chivita, sal de ahí de ese lugar");
-        String act = "la chiva";
+        String actualmente = "la chiva";
+
         for (int i = 0; i < N; i++) {
-            String prox = animales.get((int) (Math.random() * animales.size()));
-            llamarA.put(act, prox);
-            System.out.println("Hay que llamar a " + prox + " para que saque a " + act);
-            act = prox;
+            String prox = animales.get((int) (random() * animales.size()));
+            llamarA.put(actualmente, prox);
+            System.out.println("Hay que llamar a " + prox + " para que saque a " + actualmente);
+            actualmente = prox;
             List<String> remover = new ArrayList<>();
             String inspeccionar = "la chiva";
             while (llamarA.containsKey(inspeccionar)) {
@@ -43,7 +46,7 @@ public class Main {
         }
 
         long fin = System.nanoTime();
-        double tiempo = (double) ((fin - inicio)/ 1000000);
-        System.out.println("Tiempo de ejecución: " + tiempo + " milisegundos");
+        double tiempo = (double) ((fin - inicio) / 1000000);
+        System.out.println("---------------->Tiempo de ejecución: " + tiempo + " milisegundos");
     }
 }
